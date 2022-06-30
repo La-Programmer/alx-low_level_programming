a3a13e6b91e5b531773e41f88a6e4804a4a64551
La-programmer <justinoghenekomeebedi@gmail.com>
2022-06-30 13:04:05 -0700

    Get their sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,9 @@ long int l;
 long long int z;
 float f;
 printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
-printf("Size of an int: %d bytes(s)\n", (int)sizeof(i));
+printf("Size of an int: %d byte(s)\n", (int)sizeof(i));
 printf("Size of a long int: %d byte(s)\n", (int)sizeof(l));
 printf("Size of a long long int: %d byte(s)\n", (int)sizeof(z));
 printf("Size of a float: %d byte(s)\n", (int)sizeof(f));
 return (0);
 }
-
