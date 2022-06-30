eebff220a1656f4336754988f5cd8aa55ab0b47c
La-programmer <justinoghenekomeebedi@gmail.com>
2022-06-30 10:17:01 -0700

    Bug fix 2 for know their sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ long long int z;
 float f;
 printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
 printf("Size of an int: %d bytes(s)\n", (int)sizeof(i));
-printf("Size of a long int %d byte(s)\n", (int)sizeof(l));
-printf("Size of a long long int %d byte(s)\n", (int)sizeof(z));
-printf("Size of a float %d byte(s)\n", (int)sizeof(f)); 
+printf("Size of a long int: %d byte(s)\n", (int)sizeof(l));
+printf("Size of a long long int: %d byte(s)\n", (int)sizeof(z));
+printf("Size of a float: %d byte(s)\n", (int)sizeof(f));
 return (0);
 }
 
