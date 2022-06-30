5285b6df9b7b0508c1eb88cfb98802754733d8a9
La-programmer <justinoghenekomeebedi@gmail.com>
2022-06-30 10:12:34 -0700

    To know their sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - This is my main function
+ *Return: My function returns 0
+ */
+int main(void)
+{
+char c;
+int i;
+long int l;
+long long int z;
+float f;
+printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
+printf("Size of an int: %d bytes(s)\n", (int)sizeof(i));
+printf("Size of a long int %d byte(s)\n", (int)sizeof(l));
+printf("Size of a long long int %d byte(s)\n", (int)sizeof(z));
+printf("Size of a float %d byte(s)\n", (int)sizeof(f)); 
+}
+
