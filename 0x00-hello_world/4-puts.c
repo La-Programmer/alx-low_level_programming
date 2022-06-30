63e2aad92f1d2be3893383942fcd0929a807e905
La-programmer <justinoghenekomeebedi@gmail.com>
2022-06-30 08:43:42 -0700

    Bettyyyyy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  *main - Introducing my main function
+ *Return: My function returns 0
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
