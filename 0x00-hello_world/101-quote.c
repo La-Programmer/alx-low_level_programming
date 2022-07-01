f5288858f2696c2f647335db844c9208600970cd
La-programmer <justinoghenekomeebedi@gmail.com>
2022-07-01 02:53:10 -0700

    Advanced 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - This is my main function
+ *Return: my function returns 1
+ */
+int main(void)
+{
+fwrite("and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59, 1, stderr);
+return (1);
+}
