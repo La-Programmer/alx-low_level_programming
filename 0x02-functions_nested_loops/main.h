1a2104ea10313b237c94183177a685d2c9fe96fb
La-programmer <justinoghenekomeebedi@gmail.com>
2022-07-05 05:21:41 -0700

    Checking

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,32 @@
+#ifndef main.h
+#define main.h
 #include <stdio.h>
 /**
- * _putchar - My function prints 
+ * _putchar - My function prints
  * _putchar
  * Return: The function returns zero
  */
-int _putchar(void);
+
+int _putchar(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
 void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	char i;
+
+	i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+
+		i++;
+	}
+}
+
+#endif
