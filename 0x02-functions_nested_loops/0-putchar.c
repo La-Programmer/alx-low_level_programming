8142f31096f8d4a52759d1ae669469059e459357
La-programmer <justinoghenekomeebedi@gmail.com>
2022-07-05 04:31:27 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - This is my main function
+ * it prints _putchar followed by a new line
+ * Return: my function returns zero
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
