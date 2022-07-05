8c930b984eb3bad1fa93d6c176bad564d9b75637
La-programmer <justinoghenekomeebedi@gmail.com>
2022-07-05 04:42:58 -0700

    Added the main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _putchar - My function prints 
+ * _putchar
+ * Return: The function returns zero
+ */
+int _putchar(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
