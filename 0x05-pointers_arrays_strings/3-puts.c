f7b10429fe31bbabde748aed7675476070b7a117
La-programmer <justinoghenekomeebedi@gmail.com>
2022-07-13 05:14:13 -0700

    Puts function for putting them characters

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - this function reutrns a character followed
+ * by a trailing newline character.
+ * @str: the argument is any random string.
+ *
+ * Return: my function returns nothing.
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
