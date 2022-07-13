9edb224d39c58b5ccc4bec09c795859e5ea1c233
La-programmer <justinoghenekomeebedi@gmail.com>
2022-07-13 02:31:34 -0700

    Reset input to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - this function changes the value
+ * of a variable to 98 from its pointer.
+ * @n: The argument is a pointer.
+ *
+ * Return: my function does not return.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+C project for pointers arrays ad strings
