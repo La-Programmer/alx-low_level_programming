e3183900d37aaf7b7d1b7423528d03f0ce0c2b46
La-programmer <justinoghenekomeebedi@gmail.com>
2022-07-14 02:03:56 -0700

    Print every 2 characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * puts2 - this function prints every other character
+ * of a string.
+ * @str: The argument is any string.
+ *
+ * Return: the function returns nothing.
+ */
+void puts2(char *str)
+{
+	int i = strlen(str) - 1;
+
+	int a;	
+
+	for (a = 0; a < i; a += 2)
+	{
+		putchar(str[a]);
+	}
+	putchar('\n');
+}
