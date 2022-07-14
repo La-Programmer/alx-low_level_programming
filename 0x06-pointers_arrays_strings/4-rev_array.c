4bb70bef5b0208eecf02ad53cf1f7fa6785933ea
La-programmer <justinoghenekomeebedi@gmail.com>
2022-07-14 14:05:30 -0700

    Quick check

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -17,17 +17,7 @@ void reverse_array(int *a, int n)
 
 	int ind = n - 1;
 
-	int ave;
-
-	if (ind % 2 == 0)
-	{
-		ave = ind / 2;
-		ave += 1;
-	}
-	else
-	{
-		ave = (ind + 1) / 2;
-	}
+	int ave = ind / 2;
 
 	for (i = 0; i <= ave; i++)
 	{
