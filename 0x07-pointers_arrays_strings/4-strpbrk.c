89b6ea50e5d4f9327ac7e3e3178d15507bbfa1e6
La-programmer <justinoghenekomeebedi@gmail.com>
2022-07-18 22:06:12 -0700

    String break

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - this function finds the first occurence of
+ * any character of a substring in a string.
+ * @s: this argument is the string.
+ * @accept: this argument is the substring.
+ *
+ * Return: the return value is the pointer to the
+ * address of the occurence of the first character
+ * that matches the requirement.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	char *p;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		for (; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				p = &s[i];
+				for (; s[i] != '\0'; i++)
+				{
+					return (p);
+				}
+			}		
+		}
+	}
+	return (0);
+}
