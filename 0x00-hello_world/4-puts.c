39df0a4666d0e445aa494f5df5e838bab1e182f1
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-09-07 14:15:39 +0000

    Addition of script 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+* main - Print a string
+*
+* Description: Print a string using 'puts' command
+* Return: Returns 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
