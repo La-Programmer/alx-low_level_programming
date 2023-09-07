d751059d37e2286b564856e150f9409eb1df7a81
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-09-07 15:18:34 +0000

    Fix made to script 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of long int: %ld byte(s)\n", (unsigned long)sizeof(l));
-	printf("Size of long long int: %ld byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
