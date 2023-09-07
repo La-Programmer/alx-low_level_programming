ab261444c2c67d1fdcdc152ee9f637421149c5a0
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-09-07 15:10:16 +0000

    Addition of scripts 4, 5 and 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+* main - Prints a string
+*
+* Description - Print a string using only 'printf' command
+* Return: Returns 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+/**
+* main - Prints the size of different data types on the system
+* Return: Returns 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	int i;
+	long l;
+	long lli;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of long int: %ld byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of long long int: %ld byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
