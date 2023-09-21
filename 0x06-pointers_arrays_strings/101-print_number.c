8ba233b0f34d16f4fa6404d594d9180c23729101
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-09-21 20:09:12 +0000

    Addition of program 10

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_number - Prints a number
+* @n: Number to be printed
+* Return: nothing
+*/
+void print_number(int n)
+{
+	int i = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+	if (i > 9)
+		print_number(i / 10);
+
+	_putchar('0' + i % 10);
+}
