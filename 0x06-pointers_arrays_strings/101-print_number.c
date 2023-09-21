c508f8c885dd61313f04d888a66d03ad73f847c8
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-09-21 20:18:09 +0000

    Fix made to program 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,12 +7,13 @@
 void print_number(int n)
 {
 	unsigned int i = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		i = -i;
 	}
-	if (i > 9)
+	if (i / 10 != 0)
 		print_number(i / 10);
 
 	_putchar('0' + i % 10);
