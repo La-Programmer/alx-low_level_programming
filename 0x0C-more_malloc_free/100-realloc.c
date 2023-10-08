868a2753639f412567fc9fbf5f9da38b4586a9dd
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-10-08 10:41:34 +0000

    Fix made to program 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,8 +29,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	tempPtr = ptr;
-	for (i = 0; i < old_size; i++)
-		t[i] = tempPtr[i];
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			t[i] = tempPtr[i];
+	}
+	else
+	{
+		for (i = 0; i < new_size; i++)
+			t[i] = tempPtr[i];
+	}
 
 	free(ptr);
 	return (t);
