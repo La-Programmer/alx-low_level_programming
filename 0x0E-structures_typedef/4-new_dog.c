10f8f221932541fc2553ef48f078693e7b44090e
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-10-11 03:53:14 +0000

    Addition of program 4 and modified header file and fix made to program 1

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+/**
+* new_dog - Creates a new dog
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Name of dog owner
+* Return: Pointer to new_dog, NULL if Failure
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = strdup(name);
+	dog->owner = strdup(owner);
+	dog->age = age;
+
+	return (dog);
+}
