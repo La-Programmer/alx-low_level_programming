a651e890e5ef466320046b9577c1fc45b5cab0bb
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-10-19 15:02:32 +0000

    Addition of program 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* first - Execute before the main function
+* Return: Nothing
+*/
+void first(void) __attribute__((constructor));
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
