19d0694eadc0a68503f3d377305ce087402ca5a3
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-10-25 16:40:54 +0000

    Update program 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(current_node);
 		return (1);
 	}
-	while (counter != index - 1 && (current_node != NULL || current_node->next != NULL))
+	while (counter != index - 1 && current_node->next != NULL)
 	{
 		current_node = current_node->next;
 		counter++;
