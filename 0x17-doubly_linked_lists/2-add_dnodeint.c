09807cced346692aae2d7eb1f78fec5643f93886
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-12-08 12:04:09 +0000

    Addition of program 3 and update made to header file

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_dnodeint - Adds a node to the beginning of a doubly linked list
+ * @head: Pointer to the pointer to the first node of the list
+ * @n: Data for the new node
+ * Return: Address of the new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	else if (*head != NULL)
+	{
+		new_node->next = *head;
+		new_node->n = n;
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+	return (new_node);
+}
