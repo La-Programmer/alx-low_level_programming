5a54477cd52836e7736a94ff853565bd4ee3daa6
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-12-10 15:21:50 +0000

    Fix made to program 8

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,28 +15,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (new_node == NULL)
 		return (NULL);
-	while (count != idx && current != NULL)
+	new_node->n = n;
+	while (count < idx && current != NULL)
 	{
 		current = current->next;
 		count++;
 	}
 	if (idx > count)
 		return (NULL);
+	if (current == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		if (*h == NULL)
+			*h = new_node;
+		else
+		{
+			current->next = new_node;
+			current = new_node;
+		}
+	}
 	if (current->prev == NULL)
 	{
 		new_node->next = current;
 		current->prev = new_node;
 		new_node->prev = NULL;
-		new_node->n = n;
-		current = new_node;
-	}
-	else if (current->next == NULL)
-	{
-		new_node->next = NULL;
-		new_node->prev = current;
-		current->next = new_node;
-		new_node->n = n;
-		current = new_node;
+		*h = new_node;
 	}
 	else
 	{
@@ -44,8 +48,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->prev = current->prev;
 		current->prev->next = new_node;
 		current->prev = new_node;
-		new_node->n = n;
-		current = new_node;
 	}
 	return (new_node);
 }
