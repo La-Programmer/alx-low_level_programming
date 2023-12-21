8fe1ad74810ef47f31bb3277cfe071878c6c87a6
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-12-21 13:50:26 +0000

    Addition of program 1 and updated header file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creates a hash table
+ * @size: Size of the hash table
+ * Return: Pointer to hash table or NULL if failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t) * size);
+	if (table == NULL)
+		return(NULL);
+	return(table);
+}
