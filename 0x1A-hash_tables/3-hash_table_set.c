3280d610cf83405c0202c850da8797968dd58d9a
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-12-22 09:21:04 +0000

    Fix made to program 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		item->next = ht->array[index];
 		ht->array[index] = item;
-	}	
+	}
 	else
 	{
 		item->next = c_item;
