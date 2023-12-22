5c54ac3fe018d2ba97ab7e5f3abadb6def75e1b3
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-12-22 12:36:12 +0000

    Addition of program 5

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - Gets a the value of a key from the hash table
+ * @ht: Hash table
+ * @key: Key
+ *
+ * Return: Value of matching key (SUCCESS) or NULL (FAILURE)
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	const unsigned char *key2 = (const unsigned char *) key;
+	unsigned long int index = key_index(key2, ht->size);
+	hash_node_t *current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (!strcmp(current->key, key))
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
