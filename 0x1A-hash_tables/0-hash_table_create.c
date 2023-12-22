52b67b8aa7aaa9d3007f3cd68ec953e04e35e32d
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-12-22 08:45:00 +0000

    Fix to program 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,9 +8,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t) * size);
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	unsigned long int count = 0;
 
 	if (table == NULL)
 		return (NULL);
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+		return (NULL);
+	table->size = size;
+	while (count < size)
+	{
+		table->array[count] = NULL;
+		count++;
+	}
 	return (table);
 }
