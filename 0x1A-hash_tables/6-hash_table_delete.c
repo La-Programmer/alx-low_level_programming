4c127d3a64258d336fecf97e97819e170cf028a6
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-12-23 18:14:43 +0000

    Addition of program 7

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Deletes and frees a hash table
+ * @ht: Hash table
+ *
+ * Return: Nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *current;
+
+	if (ht == NULL)
+		return;
+	while (index < ht->size)
+	{
+		current = ht->array[index];
+		if (current != NULL)
+			free_list(current);
+		else
+			free(current);
+		index++;
+	}
+	free(ht);
+}
+
+/**
+ * free_list - Frees a linked list
+ * @current: Current node
+ *
+ * Return: Nothing
+ */
+void free_list(hash_node_t *head)
+{
+	hash_node_t *current = head;
+	hash_node_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		if (current->key != NULL)
+			free(current->key);
+		if (current->value != NULL)
+			free(current->value);
+		free(current);
+		current = next;
+	}
+}
