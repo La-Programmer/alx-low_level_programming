f47b4bccd837903d876259b48ffffc3ebc2b100f
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-12-23 17:21:42 +0000

    Fix made to program 6

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,6 +13,8 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int index = 0;
 	int printed = 0;
 
+	if (ht == NULL)
+		return;
 	putchar('{');
 	while (index < (ht->size))
 	{
@@ -20,10 +22,33 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (printed)
 				printf(", ");
-			printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
+			print_list(ht->array[index]);
 			printed = 1;
 		}
 		index++;
 	}
 	printf("}\n");
+}
+/**
+ * print_list - Prints a linked list
+ * @head: Pointer to the beginning of the list
+ *
+ * Return: Nothing
+ */
+void print_list(hash_node_t *head)
+{
+	int printed = 0;
+	hash_node_t *current;
+
+	if (head == NULL)
+		return;
+	current = head;
+	while (current != NULL)
+	{
+		if (printed)
+			printf(", ");
+		printf("'%s': '%s'", current->key, current->value);
+		printed = 1;
+		current = current->next;
+	}
 }
