bbcd75806cb9ca90d55e60bd664c6a39934200e3
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-08 16:32:33 +0100

    FILE: Addition of readme file, header file and script 0 which implements linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - Performs a linear search on an array to
+ * find a number
+ * @array: The array to search
+ * @size: The size of the array to search
+ * @value: The value to find
+ *
+ * Return: The first index where value os found or -1 o failure
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t counter;
+
+	if (size == 0)
+		return (-1);
+			for (counter = 0; counter < size; counter++)
+			{
+				printf("Value checked array[%lu] = [%d]\n", counter, array[counter]);
+				if (array[counter] == value)
+					return (counter);
+			}
+	return (-1);
+}
