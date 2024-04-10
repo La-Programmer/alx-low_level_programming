3761d4cd1f016d44babfbe8ff69f59dcd2139a90
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-10 23:15:18 +0100

    FILE: Addition of script 100 that implements jump search and whiteboarding directory which contains my whiteboarding text file

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+#include "search_algos.h"
+
+/**
+ * jump_search - Implementation of jump search algorithm
+ * @array: The array to search
+ * @size: The size of the array to search
+ * @value: The value to search for
+ *
+ * Return: The first index at which value is found or -1
+ * if value os not found
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	int jump_size = (int)sqrt(size);
+	size_t low = 0;
+	size_t high = jump_size;
+	int result = -1;
+	size_t i;
+
+	if (array == NULL)
+		return (result);
+	while (low < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+		if (value >= array[low] && (value <= array[high] || high > size))
+		{
+			printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+			for (i = low; i <= high && i < size; i++)
+			{
+				printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+				if (array[i] == value)
+				{
+					result = i;
+					return (result);
+				}
+			}
+			return (result);
+		}
+		else
+		{
+			low += jump_size;
+			high += jump_size;
+		}
+	}
+	return (result);
+}
