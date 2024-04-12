41f175ed6f9451f2baea2a080ce686148a43c3d6
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-12 10:32:49 +0100

    FIX: Bug fix made to script 102

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -33,17 +33,17 @@ int calc_pos(int *array, int low, int high, int value)
 int interpolation_search(int *array, size_t size, int value)
 {
 	int high = size - 1, low = 0, result = -1;
-	size_t pos;
+	size_t pos = calc_pos(array, low, high, value);
 
 	if (array == NULL)
 		return (result);
+	if (pos >= size)
+	{
+		printf("Value checked array[%lu] is out of range\n", pos);
+		return (result);
+	}
 	while (array[pos = calc_pos(array, low, high, value)] != value || high != low)
 	{
-		if (pos >= size)
-		{
-			printf("Value checked array[%lu] is out of range\n", pos);
-			return (result);
-		}
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
