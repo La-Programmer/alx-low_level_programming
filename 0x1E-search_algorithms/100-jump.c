91732ba1215b3070097a023886ccf39804b84f01
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-12 10:11:28 +0100

    FIX: Bug fix to script 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -25,7 +25,7 @@ int jump_search(int *array, size_t size, int value)
 	while (low < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", low, array[low]);
-		if (value >= array[low] && (value <= array[high] || high > size))
+		if (value >= array[low] && (value <= array[high] || high >= size - 1))
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n", low, high);
 			for (i = low; i <= high && i < size; i++)
